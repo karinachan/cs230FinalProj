f97923f19e3bfe2002e89e5e54881f4a81baa6e8
laurazeng <lzeng@wellesley.edu>
2013-11-28 16:35:04 -0500

    Update InstructionPanel.java

--- InstructionPanel.java
+++ InstructionPanel.java
@@ -7,113 +7,89 @@
 //Purpose: To create the tab where users evaluate the overall values based on their selected weighted values
 //for academic, research, and publication ratings. 
 //********************************************************************
-
 import java.awt.*;
+import java.awt.event.*; 
 import javax.swing.*;
 import javax.swing.event.*;
+import javax.swing.border.*; 
 
 public class InstructionPanel extends JPanel
 {
-   private JPanel controlsPanel, displaySchoolPanel;
-   private JSlider academicsSlider, researchSlider, publicationsSlider;
-   private JLabel academicsLabel, researchLabel, publicationsLabel, highestLabel, instructionsLabel;
-   private GradSchools grad;
+  private JPanel cards, home, buttons, game, instructs, bio;
+  private JButton start, howto, about;
+  private JLabel titleLabel;
+  //private JSlider academicsSlider, researchSlider, publicationsSlider;
+  //private JLabel academicsLabel, researchLabel, publicationsLabel, highestLabel, titleLabel;
+  
+  public InstructionPanel()
+  {
+    cards = new JPanel(new CardLayout()); 
+    home = new JPanel(new BorderLayout()); 
+    home.setBorder(new EmptyBorder(20,20,20,20));
+    buttons = new JPanel (new FlowLayout());
+    buttons.setBorder(new EmptyBorder(5,5,5,5)); 
+    titleLabel = new JLabel ("<html><strong><center>CS LAND</center>" + 
+                               "</strong><br></html>"); 
+    titleLabel.setFont(new Font("Rockwell", Font.PLAIN, 40));
+    start = new JButton ("Start"); 
+    howto = new JButton ("How To"); 
+    about = new JButton ("About Us"); 
+    start.addActionListener(new ButtonListener()); 
+    howto.addActionListener(new ButtonListener());
+    about.addActionListener(new ButtonListener());
+    home.add(titleLabel, BorderLayout.PAGE_START); 
+    buttons.add(start);
+    buttons.add(howto); 
+    buttons.add(about);
+    home.add(buttons, BorderLayout.CENTER); 
+    add(home); 
+  }
+  
+  private JPanel makeHowTo() { 
+    JPanel panel = new JPanel(); 
+    JLabel hi = new JLabel("hi"); 
+    panel.add(hi); 
+    return panel; 
+  }
+  
 
-   //-----------------------------------------------------------------
-   public EvaluationPanel(GradSchools g)
-   {
-     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); //sets layout
-     this.grad = g; 
-     
-     instructionsLabel = new JLabel ("<html><strong>Instructions:</strong> Use the sliders to determine the weight for the categories"+
-                                     " that you selected on the previous tab. <br>The school with the computed Overall rating with the selected weights will be shown."+
-                                     "<br><br></html>"); //instructions for the user
-     instructionsLabel.setFont(new Font("Rockwell", Font.PLAIN, 16));
-      academicsSlider = new JSlider (JSlider.HORIZONTAL, 0, 5, 0); //creates the sliders
-      academicsSlider.setMajorTickSpacing (1);
-      
-      academicsSlider.setPaintTicks (true);
-      academicsSlider.setPaintLabels (true);
-      academicsSlider.setAlignmentX (Component.LEFT_ALIGNMENT);
-
-      researchSlider = new JSlider (JSlider.HORIZONTAL, 0, 5, 0);
-      researchSlider.setMajorTickSpacing (1); //spaces by 1 each
-      
-      researchSlider.setPaintTicks (true);
-      researchSlider.setPaintLabels (true);
-      researchSlider.setAlignmentX (Component.LEFT_ALIGNMENT);
-
-      publicationsSlider = new JSlider (JSlider.HORIZONTAL, 0, 5, 0);
-      publicationsSlider.setMajorTickSpacing (1);
-      publicationsSlider.setPaintTicks (true);
-      publicationsSlider.setPaintLabels (true);
-      publicationsSlider.setAlignmentX (Component.LEFT_ALIGNMENT);
-
-      SliderListener listener = new SliderListener(); //sets up the listener for each slider, receives the value
-      academicsSlider.addChangeListener(listener);
-      researchSlider.addChangeListener (listener);
-      publicationsSlider.addChangeListener(listener);
-
-      academicsLabel = new JLabel ("Academics: ");
-      academicsLabel.setFont(new Font("Rockwell", Font.PLAIN, 11));
-      academicsLabel.setAlignmentX (Component.LEFT_ALIGNMENT);
-      researchLabel = new JLabel ("Research: ");
-      researchLabel.setFont(new Font("Rockwell", Font.PLAIN, 11));
-      researchLabel.setAlignmentX (Component.LEFT_ALIGNMENT);
-      publicationsLabel = new JLabel ("Publications: ");
-      publicationsLabel.setFont(new Font("Rockwell", Font.PLAIN, 11));
-      publicationsLabel.setAlignmentX (Component.LEFT_ALIGNMENT);
-
-      controlsPanel = new JPanel(); //panel for all the sliders
-      BoxLayout layout = new BoxLayout (controlsPanel, BoxLayout.X_AXIS);
-      controlsPanel.setLayout (layout);
-      controlsPanel.add (academicsLabel);
-      controlsPanel.add (academicsSlider);
-      controlsPanel.add (Box.createRigidArea (new Dimension (0, 20)));
-      controlsPanel.add (researchLabel);
-      controlsPanel.add (researchSlider);
-      controlsPanel.add (Box.createRigidArea (new Dimension (0, 20)));
-      controlsPanel.add (publicationsLabel);
-      controlsPanel.add (publicationsSlider);
-      
-      
-      
-      highestLabel= new JLabel("");
-      highestLabel.setFont(new Font("Rockwell", Font.PLAIN, 11)); //label for the school with the highest overall rating based on weighted values
-      highestLabel.setAlignmentX (Component.LEFT_ALIGNMENT);
-      
-      displaySchoolPanel = new JPanel();
-      displaySchoolPanel.add(highestLabel);
-      
-
-      add(instructionsLabel);
-      add(controlsPanel);
-      add(displaySchoolPanel);
-   
-   }
-
-   //*****************************************************************
-   //  Represents the listener for all three sliders.
-   //*****************************************************************
-   private class SliderListener implements ChangeListener
-   {
-      private int academicWeight, researchWeight, publicationsWeight;
-
-      //--------------------------------------------------------------
-      //  Gets the value of each slider, then updates the labels and
-      //  the color panel.
-      //--------------------------------------------------------------
-      public void stateChanged (ChangeEvent event)
-      {
-         academicWeight = academicsSlider.getValue(); //gets values from the slider
-         researchWeight = researchSlider.getValue();
-         publicationsWeight = publicationsSlider.getValue();
-
-         grad.computeRatings(academicWeight, researchWeight, publicationsWeight); //puts the values into the function from gradschools
-         grad.rankSchools("Overall");
-         
-         highestLabel.setText(grad.getTop().toString()); //sets the text of the highest label to the school with the highest overall ranking
-         
+  
+  private class ButtonListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      //FOR THE ADD SCHOOL BUTTON
+      if (event.getSource() == start) {
+        System.out.println("Start"); 
+        //launch the game
+      } else if (event.getSource() == howto) { 
+        System.out.println("How To"); 
+        home=makeHowTo();
+        //go to howto page
+      } else if (event.getSource() == about) { 
+        System.out.println("About"); 
+        //go to about page
       }
-   }
+    }
+  }
 }
+/*  
+  //*****************************************************************
+  //  Represents the listener for all three sliders.
+  //*****************************************************************
+  private class SliderListener implements ChangeListener
+  {
+    private int academicWeight, researchWeight, publicationsWeight;
+    
+    //--------------------------------------------------------------
+    //  Gets the value of each slider, then updates the labels and
+    //  the color panel.
+    //--------------------------------------------------------------
+    public void stateChanged (ChangeEvent event)
+    {
+      academicWeight = academicsSlider.getValue(); //gets values from the slider
+      researchWeight = researchSlider.getValue();
+      publicationsWeight = publicationsSlider.getValue();
+      
+      
+      
+    }
+  }*/
