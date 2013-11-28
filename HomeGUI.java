68c3cebf73a709c7044b3dc5e6bd979ac857d26a
laurazeng <lzeng@wellesley.edu>
2013-11-28 16:35:43 -0500

    Update HomeGUI.java

--- HomeGUI.java
+++ HomeGUI.java
@@ -1,16 +1,13 @@
 
-
 //********************************************************************
 //  GradLayout.java       Java Foundations
 //
 //********************************************************************
-
 import javax.swing.*;
+import java.awt.*; 
 
 public class HomeGUI
-{
-  //private GradSchools grad;
-  
+{  
    //-----------------------------------------------------------------
    //  Sets up a frame containing a tabbed pane. The panel on each
    //  tab demonstrates a different layout manager.
@@ -18,20 +15,14 @@ public class HomeGUI
    public static void main (String[] args)
    {
       JFrame frame = new JFrame ("HomeGUI");
-      frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-
-      Pokemon pokemon = new Pokemon();
+      frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);    
+      frame.setSize(new Dimension(500,400)); 
+      InstructionPanel panel = new InstructionPanel();
+      frame.getContentPane().add(panel);
       
-      JTabbedPane tp = new JTabbedPane(); //new tabbed pane
-      //adds each individual panel on to the pane
-      tp.addTab ("Welcome!", new WelcomePanel()); 
-      tp.addTab ("Play Game", new playPanel(pokemon));//put the buggle panel here
-      tp.addTab ("Instructions", new instructionPanel(grad));
-      tp.addTab ("About Us", new AboutPanel()); 
 
-      frame.getContentPane().add(tp);
-
-      frame.pack();
+      
+//      frame.pack();
       frame.setVisible(true);
       frame.setResizable(false); //sets it so that user cannot resize the windows because users are not trustworthy
    }
