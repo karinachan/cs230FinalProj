8a0addff24d800cc483267a8cf01627aa7e53391
karinachan <karinachan@Karinas-MacBook-Air.local>
2013-11-28 14:07:23 -0500

    Merge branch 'master' into GUI-additions
    See if this works-

--- PokemonBattle.java
+++ PokemonBattle.java
@@ -1,35 +1,50 @@
-
 import java.util.*;
 
-public class PokemonBattle
- implements Comparable<T> {
+public class PokemonBattle {
  final int loss=0; //the value of your health when you lose
  private Pokemon p1; //the self
- private Pokemon professor; //the professor
+ private Pokemon p2; //the professor
   /** Creates a pokemon game object that contains the variables associated with a game.
   */  
  
   public PokemonBattle
-(Pokemon self, Pokemon op) {
+(Pokemon self, Pokemon opp) {
     p1=self;
-    professor=op;
+    p2=opp;
     
   }
 
   public Pokemon playPokemonBattle
- (String name, int numRounds) {
-    while (p1.getHP()!=loss && professor.getHP()!=loss){ 
-      if (p1.getSPD()>=professor.getSPD()){
-        p1.attack(professor);
+ (Pokemon p1, Pokemon p2) {
+    while (p1.getHP()!=loss && p2.getHP()!=loss){ 
+      if (p1.getSPD()>=p2.getSPD( )){
+        p1.attack(p2);
       } else {
-        professor.attack(p1);
+        p2.attack(p1);
       }
     } 
-    if (p1.getHP()==0){ //returns the winner
-      return professor;} 
+    if (p1.getHP()==loss){ //returns the winner
+      return p2;} 
     else{
       return p1;}
     
+  }
+  
+  public String toString() { 
+   String s=p1.getNickName() + " is fighting against " + p2.getNickName() + ".\n"; 
+   s+=playPokemonBattle(p1, p2).getNickName() + " wins!";
+//   String s=p2.getTrainer() + " wants to fight!" +
+//    p2.getTrainer() + " sent out " + p2.getNickName() + "!" + 
+//    "Go! " + p1.getNickName() + "!"; 
+   
+   return s; 
+  }
+  
+  public static void main (String[] args) { 
+   Pokemon lyn = new Pokemon ("Buneary", "Angelica", "Lyn", 100, 20, 50); 
+   Pokemon rhys = new Pokemon ("Mr. Mime", "Mime", "Rhys", 100, 30, 40); 
+   PokemonBattle fight = new PokemonBattle(lyn, rhys);
+   System.out.println(fight); 
   }
   
   //if lose, then set the pokemon obj (if you lost) back a node on the tree of opponents
@@ -37,11 +52,3 @@ public class PokemonBattle
   //increment the win for the pokemon 
   //increment number of people you've visited 
   
-  
-  /** Start the homework by reading this method. 
-    */
-  public static void main (String args[]) {   
-    // Create an instance of a new game and play the rounds
-    
-  }
-}
