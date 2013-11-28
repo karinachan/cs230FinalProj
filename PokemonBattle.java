802759c52990f085db637179e535fbd3c03c01fc
laurazeng <lzeng@wellesley.edu>
2013-11-28 13:21:09 -0500

    Update PokemonBattle.java
    updated some variable names so that they're more intuitive
    also working on the toString method (need to make changes to Pokemon class before I can finish)

--- PokemonBattle.java
+++ PokemonBattle.java
@@ -1,35 +1,45 @@
-
 import java.util.*;
 
 public class PokemonBattle
  implements Comparable<T> {
  final int loss=0; //the value of your health when you lose
  private Pokemon p1; //the self
- private Pokemon professor; //the professor
+ private Pokemon p2; //the professor
   /** Creates a pokemon game object that contains the variables associated with a game.
   */  
  
   public PokemonBattle
-(Pokemon self, Pokemon op) {
+(Pokemon self, Pokemon opp) {
     p1=self;
-    professor=op;
+    p2=opp;
     
   }
 
   public Pokemon playPokemonBattle
  (String name, int numRounds) {
-    while (p1.getHP()!=loss && professor.getHP()!=loss){ 
-      if (p1.getSPD()>=professor.getSPD()){
-        p1.attack(professor);
+    while (p1.getHP()!=loss && p2.getHP()!=loss){ 
+      if (p1.getSPD()>=p2.getSPD()){
+        p1.attack(p2);
       } else {
-        professor.attack(p1);
+        p2.attack(p1);
       }
     } 
-    if (p1.getHP()==0){ //returns the winner
-      return professor;} 
+    if (p1.getHP()==loss){ //returns the winner
+      return p2;} 
     else{
       return p1;}
     
+  }
+  
+  public String toString() { 
+   String s="hi"; 
+   return s; 
+  }
+  
+  public static void main (String[] args) { 
+   String s=p2.getTrainer() + " wants to fight!" +
+    p2.getTrainer() + " sent out " + p2.getNickName() + "!" + 
+    "Go! " + p1.getNickName() + "!"; 
   }
   
   //if lose, then set the pokemon obj (if you lost) back a node on the tree of opponents
