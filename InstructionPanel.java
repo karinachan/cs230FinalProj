5bde13db15dd606bb8fa2b1c316b6ee6e2a55a55
karinachan <karinachan@Karinas-MacBook-Air.local>
2013-12-19 11:15:51 -0500

    InstructionPanel/dont forget to add html code diploma.

--- InstructionPanel.java
+++ InstructionPanel.java
@@ -6,61 +6,27 @@ import javax.swing.event.*;
 import javax.swing.border.*; 
 
 public class InstructionPanel extends JPanel{
-  private JPanel instrpanel;
-  private JButton instrback;
-  private LayoutPanel layout;
+
 
   public InstructionPanel(){
+    setLayout(new FlowLayout()); //sets the layout to border layout
+    setBackground (new Color (255, 255, 255)); //sets the color background
+    
+    //creates labels with text on them
+    JLabel l1 = new JLabel ("<html> <strong> HOW TO PLAY:</strong><br>");
+    l1.setFont(new Font("Rockwell", Font.PLAIN, 16));
+   
+    JLabel l2 = new JLabel ("1. Click the A button to interact with characters. <br>2. To battle, go up to the professor and press A."+
+    "<br>3. If you win, a pop up will tell you so and you will progress to the next professor.<br>4. If you do not, rebattle the professor."+
+    "<br>5. When you win, check the directory for a diploma that you can print out!");
+   l2.setFont(new Font("Rockwell", Font.PLAIN, 10));
+    add (l1);
+    add (l2);
 
-    JLabel hi= new JLabel("TOMATOES");
-    instrpanel = new JPanel(new FlowLayout()); 
-    
-    instrpanel.add(hi); 
-    instrback = new JButton("Back"); 
-    //instrback.addActionListener(new ButtonListener(instrback)); 
-    instrpanel.add(instrback);
-    
-    instrpanel.setVisible(false); //hidden until pressed
-    
+  }
   }
   
-  public JPanel getPanel(){
-    
-    return instrpanel;
-  }
   
-   public JButton getButton(){
-     return instrback;
-   }
-  /*
-  private class ButtonListener implements ActionListener {
-    
-    // LayoutPanel layout;
-    
-    JButton set;
-    
-    public ButtonListener(JButton button){
-      
-      
-      set=button;
-    }
-    
-    
-    
-    
-    public void actionPerformed(ActionEvent event){
-      //FOR THE ADD SCHOOL BUTTON
-      if (event.getSource()==set){
-        instrpanel.setVisible(false);
-        layout.getPanel().setVisible(true);
-        
-        
-        
-      } 
-      
-      
-    }*/
-  }
   
   
   
