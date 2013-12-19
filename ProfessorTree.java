e0879d7ed32f72aa3f5cd95b263c223cd47d2bdf
laurazeng <lzeng@wellesley.edu>
2013-12-19 11:55:05 -0500

    Update ProfessorTree.java

--- ProfessorTree.java
+++ ProfessorTree.java
@@ -1,17 +1,24 @@
+/** Karina Chan and Laura Zeng
+  * CS 230 
+  * Final Project
+  * ProfessorTree.java
+  * 
+  */
+
 import java.util.*;
 import javafoundations.*;
 import javafoundations.LinkedQueue; //for BFS
 import javafoundations.ArrayStack; //for DFS
 import java.util.*;
 import java.io.*;
+import java.awt.*;
+import javax.imageio.*; 
 
 public class ProfessorTree<T> implements Iterable<T>{
   private T[] tree; 
   private int count; 
   private final int NOT_FOUND=-1;
   private Object top;
-
-
   
   public ProfessorTree (T root) { 
     top= root;
@@ -19,7 +26,14 @@ public class ProfessorTree<T> implements Iterable<T>{
     count=1; 
   }
   
-  //  Expands T[] tree when it's too small to accomodate new family
+  public ProfessorTree() { 
+    //empty constructor
+    //used so we can invoke createOpponents() and make the
+    //professor tree that's used in the game
+  }
+  
+  /**  expands T[] tree when it's too small to accomodate new family
+    */
   private void expand() { 
     //decided to expand by 2 times + 1 whenever necessary (this  
     //works well b/c each new generation has as many members as 
@@ -31,13 +45,18 @@ public class ProfessorTree<T> implements Iterable<T>{
     tree=temp; 
   }
   
-  //  Returns the element stored in the root (aka CoTU) of the tree.
+  /** returns the element stored in the root (aka CoTU) of the tree.
+    * @return tree[0]    the "boss" or KBot
+    */
   public T getBoss() { 
     return tree[0]; 
   }
   
-  //  indexOf() finds the index of the T target in the T[] tree
-  //  it returns NOT_FOUND if the target does not exist in T[] tree
+  /**  indexOf() finds the index of the T target in the T[] tree
+    * it returns NOT_FOUND if the target does not exist in T[] tree
+    * @param target           what you're looking for
+    * @return i, NOT_FOUND    where it is
+    */
   private int indexOf (T target) { 
     for (int i=0; i<tree.length; i++) { 
       if (tree[i]!=null && tree[i].equals(target)) { 
@@ -47,8 +66,11 @@ public class ProfessorTree<T> implements Iterable<T>{
     return NOT_FOUND; 
   }
   
-  //  Returns the member that is the offspring of target.
-  //  Returns null as the offspring of the root.
+  /**  Returns the member that is the offspring of target.
+    * Returns null as the offspring of the root.
+    * @param target             the Pokemon that you want to find the offspring of
+    * @return tree[i], null     the offspring that is found
+    */
   public T getNextProf(T target) { 
     try { 
       //returns the T object stored in the ((index+1)/2)-1 slot 
@@ -59,19 +81,15 @@ public class ProfessorTree<T> implements Iterable<T>{
       //doesn't work for index 0
       return tree[((indexOf(target)+1)/2)-1];
     } catch (ArrayIndexOutOfBoundsException aioobe) { 
-      /*if (getBoss().equals(target)) { 
-        System.out.println("The target " + target + " does not have offspring."); 
-      } else { 
-        //if the target isn't the CoTU, and the program is running into aioobe, 
-        //then that means the target does not exist
-        System.out.println("The target " + target + " does not exist."); 
-      }*/
       return null; 
     }
   }
   
-  //  Returns the member that is the left child of target.
-  //  Returns null if the left child does not exist.
+  /** Returns the member that is the left child of target.
+    * Returns null if the left child does not exist.
+    * @param target             the Pokemon that you want to find the child of
+    * @return tree[i], null     the child that you find
+    */
   public T getLeft(T target) { 
     //(indexOf(target)*2)+1 is the left-side child (Tree term)
     //if the index of the left-side child (Tree term is empty, 
@@ -83,8 +101,10 @@ public class ProfessorTree<T> implements Iterable<T>{
     }
   }
   
-  //  Sets the left child of the tree target to lprof.
-  //  It throws an exception if target is not already in the tree
+  /** Sets the left child of the tree target to lprof.
+    * It throws an exception if target is not already in the tree
+    * @param target, lprof   the Pokemon you're using as the base, the Pokemon you're setting
+    */
   public void setLeft(T target, T lprof) { 
     try { 
       //checks to see if target exists
@@ -109,11 +129,13 @@ public class ProfessorTree<T> implements Iterable<T>{
     } catch (ArrayIndexOutOfBoundsException aioobe) { 
       //System.out.println("The target " + target + " does not exist.");
     }
-    
   }
   
-  //  Returns the element that is the right child of target.
-  //  Returns null if the right child does not exist
+  /** Returns the element that is the right child of target.
+    * Returns null if the right child does not exist
+    * @param target             the Pokemon that you want to find the child of
+    * @return tree[i], null     the child that you find
+    */
   public T getRight(T target) { 
     //(indexOf(target)*2)+2 is the right-side child (Tree term)
     //if the index of the right-side child (Tree term is empty, 
@@ -125,8 +147,10 @@ public class ProfessorTree<T> implements Iterable<T>{
     }
   }
   
-  //  Sets the right child of target to rprof.
-  //  It throws an exception if target is not already in the tree
+  /** Sets the right child of target to rprof.
+    * It throws an exception if target is not already in the tree
+    * @param target, lprof    the Pokemon you're using as the base, the Pokemon you're setting
+    */
   public void setRight(T target, T rprof) {
     try { 
       //checks to see if target exists
@@ -153,8 +177,11 @@ public class ProfessorTree<T> implements Iterable<T>{
     }
   }
   
-  //  Returns true if the tree contains an element that
-  //  matches the specified target element and false otherwise.
+  /** Returns true if the tree contains an element that
+    * matches the specified target element and false otherwise.
+    * @param target                Pokemon you want to find
+    * @return indexOf(target)>0    whether or not it exists
+    */
   public boolean appears (T target) {
     //if the target exists in the array, then it must have an index 
     //so, as long as indexOf() produces something other than -1, 
@@ -162,9 +189,12 @@ public class ProfessorTree<T> implements Iterable<T>{
     return (indexOf(target)>=0); 
   }
   
-  // Returns true if the two members share a grandchild,
-  // and false if they are not or if a shared grandchild does not exist
-  // Two grandparents that share a grandchild are "inLaws"
+  /** Returns true if the two members share a grandchild,
+    * and false if they are not or if a shared grandchild does not exist
+    * Two grandparents that share a grandchild are "inLaws"
+    * @param gp1, gp2          the two potential inlaws
+    * @return true, false      whether or not they are inlaws
+    */
   public boolean inLaws(T gp1, T gp2) { 
     try { 
       return (getNextProf(getNextProf(gp1)).equals(getNextProf(getNextProf(gp2)))); 
@@ -175,13 +205,17 @@ public class ProfessorTree<T> implements Iterable<T>{
     }
   }
   
-  //  Returns the number of members in this ancestral tree.
+  /** Returns the number of members in this ancestral tree.
+    * @return count
+    */
   public int size() { 
     return count; 
   }
   
-  //  Returns the string representation of the binary tree,
-  //  one line per level.
+  /** Returns the string representation of the binary tree,
+    * one line per level.
+    * @return s    string representation of ProfessorTree
+    */
   public String toString() { 
     String s = "The CS department has " + count + " members:\n"; 
     Iterator<T> iter = iterator();
@@ -207,16 +241,17 @@ public class ProfessorTree<T> implements Iterable<T>{
     return s;   
   }
   
-  
-  //maybe write remove node 
-  
-  //  Set to return a level-order traversal of the tree (byGenerations())
+  /** Set to return a level-order traversal of the tree (byGenerations())
+    * @return byGenerations()     the by generations iterator
+    */
   public Iterator<T> iterator() { 
     return byGenerations();
   }
   
-  //  Returns an iterator that contains a level-order traversal
-  //  on the ancestral tree.
+  /** Returns an iterator that contains a level-order traversal
+    * on the ancestral tree.
+    * @return iter          the level order iterator of the tree
+    */
   public Iterator<T> byGenerations() {
     LinkedQueue<BTNode<T>> queue = new LinkedQueue<BTNode<T>>();
     ArrayIterator<T> iter = new ArrayIterator<T>();
@@ -240,52 +275,65 @@ public class ProfessorTree<T> implements Iterable<T>{
     return iter;
   }
   
-    public ProfessorTree<String> createsGameTree(){ //pokemon
-    ProfessorTree<String> fam = new ProfessorTree<String>("KBot"); 
-    //ordered by CS classes taught- and then arbitrarily ordered because there overlap in the dept..
-    fam.setLeft("KBot", "Ellen"); 
-    fam.setRight("KBot", "Randy"); 
-    fam.setLeft("Ellen", "Scott"); 
-    fam.setRight("Ellen", "Lyn"); 
-    fam.setLeft("Scott", "Rhys"); 
-    fam.setRight("Scott", "Darakhshan"); 
-    fam.setLeft("Lyn", "Eni"); 
-    fam.setRight("Lyn", "Rita"); 
-    fam.setLeft("Randy", "Takis"); 
-    fam.setRight("Randy", "Orit"); 
-    fam.setLeft("Takis", "Stella"); 
-    fam.setRight("Takis", "Brian"); 
-    fam.setLeft("Orit", "Jean"); 
-    fam.setRight("Orit", "Sohie"); 
-    //System.out.println("fam.getRight("Orit"));
-    //System.out.println(fam); 
-    //System.out.println(fam.shuffle());
-    //System.out.println(fam); 
-    fam.shuffle(); 
-    //System.out.println(fam);
-    return fam; 
-  }
+  /** creates the ProfessorTree that is used in the game
+    * @return fam       the ProfessorTree that is used in game
+    */
+  public ProfessorTree<Pokemon> createOpponents() throws IOException {
+    Image sprite1 = ImageIO.read(new File("prof1.png"));
+    Image sprite2 = ImageIO.read(new File("prof2.png"));
+    Image sprite3 = ImageIO.read(new File("prof3.png"));
+    Image sprite4 = ImageIO.read(new File("prof4.png"));
     
+    Pokemon kBot= new Pokemon("Mewtwo", "Wendy","KBot", 100, new Image[]{sprite3});
+    Pokemon randy= new Pokemon("Dewgong", "CS240","Randy", new Image[]{sprite4});
+    Pokemon ellen= new Pokemon("Nidoqueen", "CS Dept Chair","Ellen", new Image[]{sprite1});
+    Pokemon scott= new Pokemon("Diglett", "Doge","Scott", new Image[]{sprite2});
+    Pokemon lyn= new Pokemon("Eevee", "Angelica","Lyn", new Image[]{sprite3});
+    Pokemon rhys= new Pokemon("Mr. Mime", "Scheme","Rhys", new Image[]{sprite4});
+    Pokemon darakhshan= new Pokemon("Abra", "Security","Darakhshan", new Image[]{sprite1});
+    Pokemon eni= new Pokemon("Ekans", "Python","Eni", new Image[]{sprite2});
+    Pokemon rita= new Pokemon("Oddish", "SoOdd","Rita", new Image[]{sprite3});
+    Pokemon takis= new Pokemon("Wartortle", "Captain Guy","Takis", new Image[]{sprite4});
+    Pokemon stella= new Pokemon("Seadra", "Ocean","Stella", new Image[]{sprite1});
+    Pokemon sohie= new Pokemon("Lapras", "Lucker","Sohie", new Image[]{sprite2});
+    Pokemon brian= new Pokemon("Dratini", "Dracomp","Brian", new Image[]{sprite3});
+    Pokemon jean= new Pokemon("Zapdos", "Gates","Jean", new Image[]{sprite4});
+    Pokemon orit= new Pokemon("Porygon", "HCI","Orit", new Image[]{sprite1});
+
+    ProfessorTree fam = new ProfessorTree(kBot); 
+    fam.setLeft(kBot, ellen); 
+    fam.setRight(kBot, randy); 
+    fam.setLeft(ellen, scott); 
+    fam.setRight(ellen, lyn); 
+    fam.setLeft(scott, rhys); 
+    fam.setRight(scott, darakhshan); 
+    fam.setLeft(lyn, eni); 
+    fam.setRight(lyn, rita); 
+    fam.setLeft(randy, takis); 
+    fam.setRight(randy, orit); 
+    fam.setLeft(takis, stella); 
+    fam.setRight(takis, brian); 
+    fam.setLeft(orit, jean); 
+    fam.setRight(orit, sohie);  
+    
+    fam.shuffle(); 
+    return fam;
+  }
+  
+  /** shuffles the ProfessorTree (random gameplay every time)
+    * @return profs    the ProfessorTree (post-shuffle) that invokes this method
+    */
   public ProfessorTree<T> shuffle(){
     ProfessorTree shuffled= new ProfessorTree (top);
-    //T[] profs = (T[])new Object[count]; 
     T[] profs = tree; 
     Random r = new Random(); 
-    
-   
+    //make a temp, then swap
     for (int i=1; i<count; i++) { 
       int rand = r.nextInt(i)+1; //offset so that 0 is not included (shuffling everything that is not kbot) 
       T hold = profs[rand]; 
       profs[rand]=profs[i]; 
-      profs[i]=hold; 
-      
-    
+      profs[i]=hold;     
     }
-  /*
-    for (int i=0; i<count; i++) { 
-      System.out.println(profs[i]); 
-    }
-    */ //this used to print out the professors in order
     return shuffled;
   }
      
@@ -307,9 +355,6 @@ public class ProfessorTree<T> implements Iterable<T>{
     fam.setRight("Takis", "Brian"); 
     fam.setLeft("Orit", "Jean"); 
     fam.setRight("Orit", "Sohie"); 
-    //System.out.println("fam.getRight("Orit"));
-    //System.out.println(fam); 
-    //System.out.println(fam.shuffle());
     System.out.println(fam); 
     fam.shuffle(); 
     System.out.println(fam);
