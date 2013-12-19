d1059e696efdd3dcde8957ace9f154557abf1592
laurazeng <lzeng@wellesley.edu>
2013-12-19 11:54:05 -0500

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,8 +1,27 @@
+/** Karina Chan and Laura Zeng
+  * CS 230 
+  * Final Project
+  * Pokemon.java
+  * 
+  * Creates a Pokemon object, which represents the Pokemon and the trainer.
+  * We chose to have both the Pokemon and the trainer in one class because the
+  * trainer doesn't do anything except provide a name and a sprite, so it was
+  * more or less just extra information tacked on to a Pokemon. 
+  * Pokemon have a name (the actual Pokemon name), nickname (the name a trainer
+  * chooses for the Pokemon), HP, Attack, Speed, and trainer. The trainer also 
+  * comes with sprites that are later used to draw the Pokes (which are basically Buggles). 
+  * It's important to have the sprites in the Pokemon class because Pokes are moving
+  * containers for Pokemon and a new Poke is made each time (leading to random sprites for
+  * the professors), while Pokemon (taken in as a parameter) remains constant. The Pokemon
+  * also has a WonList (all the professors that it's won against) and a VisitList (all 
+  * the professors that it's visited). 
+  * 
+  */
+
 import java.util.*; 
 import java.io.*;
 import java.awt.*; 
 import javax.imageio.*; 
-
 import javafoundations.*;
 
 public class Pokemon { 
@@ -12,14 +31,12 @@ public class Pokemon {
   private int atk; 
   private int spd; 
   private String trainer;
-  private ProfessorTree <Pokemon> fam; 
   private ArrayStack wonList; //here so that every battle, the pokemon keeps same stats (easier to save also)
   private ArrayStack visitList; 
   private Image[] sprites; 
   
   public Pokemon (String name, String nickName, String trainer, Image[] imgs) { 
     Random rand = new Random();
-    
     this.name=name; 
     this.nickName=nickName; 
     this.trainer=trainer; 
@@ -30,22 +47,21 @@ public class Pokemon {
     visitList = new ArrayStack();
     sprites = imgs; 
   }
-
+  
   public Pokemon (String name, String nickName, String trainer, 
                   int hp, int atk, int spd, Image[] imgs) { 
     this(name, nickName, trainer, imgs);
     this.hp=hp;
     this.atk=atk; 
     this.spd=spd; 
-        wonList= new ArrayStack(); //no one in this stack yet.
+    wonList= new ArrayStack(); //no one in this stack yet.
     visitList = new ArrayStack();
     sprites = imgs; 
-
+    
   }
-    public Pokemon (String name, String nickName, String trainer, int spd, Image[] imgs){
+  public Pokemon (String name, String nickName, String trainer, int spd, Image[] imgs){
     this(name, nickName, trainer, imgs);
     Random rand = new Random();
-    
     //rewrites the previous numbers in the constructors 
     this.hp= rand.nextInt(700)+800; //random between 700-1500
     this.atk= rand.nextInt(150)+150; //random between 150-300
@@ -54,120 +70,114 @@ public class Pokemon {
     sprites=imgs; 
   }
   
-    public Image[] getSprites() { 
-      return sprites; 
-    }
-
+  /** gets the sprites
+    * @return sprites
+    */
+  public Image[] getSprites() { 
+    return sprites; 
+  }
+  
+  /** gets the wonList
+    * @return wonList
+    */
   public ArrayStack getWon(){
     return wonList;
   }
   
+  /** gets size of wonList
+    * @return wonList.size()
+    */
   public int getWonSize(){
     return wonList.size();
   }
   
+  /** gets visitList
+    * @return visitList
+    */
   public ArrayStack getVisit() { 
     return visitList; 
   }
+  
+  /** gets size of visitList
+    * @return visitList.size()
+    */
   public int getVisitSize() { 
     return visitList.size(); 
   }
   
-  
-  
-  public ProfessorTree<Pokemon> createOpponents() throws IOException {
-    Image sprite1 = ImageIO.read(new File("prof1.png"));
-    Image sprite2 = ImageIO.read(new File("prof2.png"));
-    Image sprite3 = ImageIO.read(new File("prof3.png"));
-    Image sprite4 = ImageIO.read(new File("prof4.png"));
-    
-    Pokemon kBot= new Pokemon("Mewtwo", "Wendy","KBot", 100, new Image[]{sprite3});
-    Pokemon randy= new Pokemon("Dewgong", "CS240","Randy", new Image[]{sprite4});
-    Pokemon ellen= new Pokemon("Nidoqueen", "CS Dept Chair","Ellen", new Image[]{sprite1});
-    Pokemon scott= new Pokemon("Diglett", "Doge","Scott", new Image[]{sprite2});
-    Pokemon lyn= new Pokemon("Eevee", "Angelica","Lyn", new Image[]{sprite3});
-    Pokemon rhys= new Pokemon("Mr. Mime", "Scheme","Rhys", new Image[]{sprite4});
-    Pokemon darakhshan= new Pokemon("Abra", "Security","Darakhshan", new Image[]{sprite1});
-    Pokemon eni= new Pokemon("Ekans", "Python","Eni", new Image[]{sprite2});
-    Pokemon rita= new Pokemon("Oddish", "SoOdd","Rita", new Image[]{sprite3});
-    Pokemon takis= new Pokemon("Wartortle", "Captain Guy","Takis", new Image[]{sprite4});
-    Pokemon stella= new Pokemon("Seadra", "Ocean","Stella", new Image[]{sprite1});
-    Pokemon sohie= new Pokemon("Lapras", "Lucker","Sohie", new Image[]{sprite2});
-    Pokemon brian= new Pokemon("Dratini", "Dracomp","Brian", new Image[]{sprite3});
-    Pokemon jean= new Pokemon("Zapdos", "Gates","Jean", new Image[]{sprite4});
-    Pokemon orit= new Pokemon("Porygon", "HCI","Orit", new Image[]{sprite1});
-
-    
-    
-    fam= new ProfessorTree(kBot); //creates the hierarchy (changes every game)
-    fam.setLeft(kBot, ellen); 
-    fam.setRight(kBot, randy); 
-    fam.setLeft(ellen, scott); 
-    fam.setRight(ellen, lyn); 
-    fam.setLeft(scott, rhys); 
-    fam.setRight(scott, darakhshan); 
-    fam.setLeft(lyn, eni); 
-    fam.setRight(lyn, rita); 
-    fam.setLeft(randy, takis); 
-    fam.setRight(randy, orit); 
-    fam.setLeft(takis, stella); 
-    fam.setRight(takis, brian); 
-    fam.setLeft(orit, jean); 
-    fam.setRight(orit, sohie);  
-    
-    fam.shuffle(); 
-    
-    
-    //System.out.println(fam);
-    return fam;
-  }
-  
+  /** gets name of Pokemon
+    * @return name
+    */
   public String getName() { 
     return name; 
   } 
+  
+  /** gets nickName of Pokemon
+    * @return nickName
+    */
   public String getNickName() { 
     return nickName; 
   }
+  
+  /** gets trainer of Pokemon
+    * @return trainer
+    */
   public String getTrainer() { 
     return trainer; 
   }
+  
+  /** gets HP of Pokemon
+    * @return hp
+    */
   public int getHP() { 
     return hp; 
   }
+  
+  /** gets attack of Pokemon 
+    * @return atk
+    */
   public int getATK() { 
     return atk; 
   } 
+  
+  /** gets speed of Pokemon 
+    * @return spd
+    */
   public int getSPD() { 
     return spd; 
   }
   
+  /** sets HP of Pokemon to backup
+    * @param backup
+    */
   public void setHP(int backup){
-    this.hp = backup; //since the original will change (if you get here, you might get the value of the damaged one + bonus) 
+    this.hp = backup; //since the original will change (if you get here, 
+                      //you might get the value of the damaged one + bonus) 
   }
   
+  /** sets attack of Pokemon to current attack + increase
+    * @param increase    amount that your attack increases
+    */
   public void setATK(int increase){
     this.atk= this.getATK()+increase;
   }
   
-  //this is the attacking pokemon, and p is the pokemon being attacked
-  //this produces a String that is used in attack() in PokemonBattle so 
-  //that the user is able to track the progress of the battle
-  //jk, no need for Strings
+  /** simulates one Pokemon (this) attacking another (p)
+    * crit is a random integer (1-20) that gets added on to Pokemon this's 
+    * attack base stat (which allows for more varied gameplay)
+    * @param p     your opponent
+    */
   public void attack(Pokemon p) {
-    //String s=this.getNickName() + " attacked " + p; 
-    //int pre_hp=p.hp; 
-    //adds more randomness to the battle, making it more exciting
     Random rand = new Random(); 
     int crit = rand.nextInt(20);
-    System.out.println("The crit is: " + crit); 
+    //System.out.println("The crit is: " + crit); 
     int dmg = atk+crit; 
     p.hp-=dmg; 
-    //s+=p + "'s HP dropped from " + pre_hp + " to " + p.hp + "."; 
-    //return s; 
   }
   
-  
-  
+  /** String representation of Pokemon 
+    * @return s
+    */
   public String toString() { 
     String s="\nPokemon: " + name + 
       "\nNickname: " + nickName +
@@ -179,12 +189,10 @@ public class Pokemon {
   }
   
   public static void main (String[] args) throws IOException { 
-    Image sprite1 = ImageIO.read(new File("prof1.png"));
-    
+    Image sprite1 = ImageIO.read(new File("prof1.png"));  
     Pokemon p = new Pokemon ("Buneary", "Angelica", "Lyn", new Image[]{sprite1}); 
-    //System.out.println(p); 
+    System.out.println(p); 
     //p.createOpponents();
-    System.out.println(p.createOpponents());
   }
   
 }
